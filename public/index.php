e3b0a7967736ce35da58cea9863aad70f35bb4a7
Arthur Parent <artpare4@gmail.com>
2023-05-05 11:28:17 +0200

    Mise à jour du fichier composer.json et modification du fichier index.php pour y affichier la liste des artistes de la base de donnée

--- public/index.php
+++ public/index.php
@@ -1,5 +1,37 @@
 <?php
+declare(strict_types=1);
 
+require_once '../vendor/autoload.php';
+use Database\MyPdo;
+$html=<<<HTML
+    <!DOCTYPE html>
+    <html>
+    
+        <head>
+            <title>Liste des artistes</title>
+        </head>
+        <body> \n
+HTML;
 
-echo 'Hello Music!';
+MyPDO::setConfiguration('mysql:host=mysql;dbname=cutron01_music;charset=utf8', 'web', 'web');
 
+$stmt = MyPDO::getInstance()->prepare(
+    <<<'SQL'
+    SELECT id, name
+    FROM artist
+    ORDER BY name
+SQL
+);
+
+$stmt->execute();
+
+while (($ligne = $stmt->fetch()) !== false) {
+    $html.= "<p>{$ligne['name']}</p>\n";
+}
+
+$html.=<<<HTML
+    </body>
+</html>
+HTML;
+
+echo $html;
