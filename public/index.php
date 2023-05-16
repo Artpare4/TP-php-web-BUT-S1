6916851743a479c64775ccd5eeef22082292df1c
pare0028 <arthur.parent@etudiant.univ-reims.fr>
2023-05-16 15:24:12 +0200

    Refactorisation de index.php avec les classes artistes et artistesCollection

--- public/index.php
+++ public/index.php
@@ -4,25 +4,18 @@ declare(strict_types=1);
 
 use Database\MyPdo;
 use Html\WebPage;
-
+use Entity\Artist;
+use Entity\Collection\ArtistCollection;
 $webPage=new WebPage();
 
 $webPage->setTitle("Liste des artistes");
 
-
-$stmt = MyPDO::getInstance()->prepare(
-    <<<'SQL'
-    SELECT id, name
-    FROM artist
-    ORDER BY name
-SQL
-);
-
-$stmt->execute();
+$Artistes= new ArtistCollection();
+$liste_Artistes=$Artistes->findAll();
 $content="";
 
-while (($ligne = $stmt->fetch()) !== false) {
-    $content.= "<a href='http://localhost:8000/artist.php?artistId={$ligne['id']}'>{$webPage->escapeString($ligne['name'])}</a><br>";
+foreach ($liste_Artistes as $artiste) {
+    $content .= "<a href='http://localhost:8000/artist.php?artistId={$artiste->getId()}'>{$webPage->escapeString($artiste->getName())}</a><br>";
 }
 $webPage->appendContent($content);
 echo $webPage->toHTML();
