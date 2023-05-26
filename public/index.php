5be092e7b904a42632cb22311fc29dddd9bbcd69
pare0028 <arthur.parent@etudiant.univ-reims.fr>
2023-05-26 09:57:13 +0200

    Modification css (Modification liste des Artistes)

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ $liste_Artistes=$Artistes->findAll();
 $content="";
 
 foreach ($liste_Artistes as $artiste) {
-    $content .= "<a href='http://localhost:8000/artist.php?artistId={$artiste->getId()}'>{$webPage->escapeString($artiste->getName())}</a><br>";
+    $content .= "<a class='artist' href='http://localhost:8000/artist.php?artistId={$artiste->getId()}' >{$webPage->escapeString($artiste->getName())}</a>";
 }
 
 $webPage->appendContent($content);
